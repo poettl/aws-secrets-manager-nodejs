d2e28fae9794592a0c20e968443ab997a01c9b04
Peter Öttl <hello@peteroettl.com>
2024-01-17 12:11:27 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # aws-secrets-manager-nodejs
+
+This is a simple cli tool to get the value of a secret from AWS Secrets Manager.
+It will generate a file called .env in the current directory with the key value pairs of the secret.
+
+## Usage
+
+clone the repo
+cd into the repo
+npm install -g
+aws-secrets-manager-nodejs --region eu-central-1 --secretId your-secret-id
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
+const yargs = require("yargs");
+
+const argv = yargs
+  .option("region", {
+    description: "AWS region",
+    type: "string",
+    demandOption: true,
+  })
+  .option("secretId", {
+    description: "SecretId in AWS Secrets Manager",
+    type: "string",
+    demandOption: true,
+  }).argv;
+
+// Configure AWS SDK
+const region = argv.region;
+const client = new SecretsManager({
+  region,
+});
+const secretId = argv.secretId;
+
+return new Promise((resolve, reject) => {
+  // Retrieving secrets from Secrets Manager
+  client.getSecretValue({ SecretId: secretId }, (err, data) => {
+    if (err) {
+      console.error("Error occurred");
+      reject(err);
+    } else {
+      // Parsing the fetched data into JSON
+      const secretsJSON = JSON.parse(data.SecretString);
+
+      let secretsString = "";
+      Object.keys(secretsJSON).forEach((key) => {
+        secretsString += `${key}=${secretsJSON[key]}\n`;
+      });
+
+      // Write to .env file
+      fs.writeFile(".env", secretsString, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve("Secrets successfully written to .env file");
+        }
+      });
+    }
+  });
+});
